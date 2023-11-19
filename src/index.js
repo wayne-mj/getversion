03c6992e12c117acc9acce43e2046ee9a00580e9
Wayne Jackson <warpkez@hotmail.com>
2023-11-19 19:43:17 +1000

    Added semver support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const semver = require('semver');
+
 const GetVersion = require('./checkversiontag');
 
 if (require.main === module)
